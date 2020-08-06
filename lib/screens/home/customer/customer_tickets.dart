432949c7c45563906c454dc1edb5712ccf73d984
adithya-tp <adithyatp@yahoo.com>
2020-08-06 15:55:23 +0400

    Wrapped in List View to prevent renderflow error

--- lib/screens/home/customer/customer_tickets.dart
+++ lib/screens/home/customer/customer_tickets.dart
@@ -17,129 +17,131 @@ class _CustomerTicketState extends State<CustomerTicket> {
   Widget build(BuildContext context) {
     User user = Provider.of<User>(context);
 
-    return Scaffold(
-      body: Container(
-        margin: EdgeInsets.all(10.0),
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          children: <Widget>[
-            Container(
-              child: StreamBuilder(
-                //future: getNearby(),
-                stream: DatabaseService(uid: user.userId).customerTickets,
-                initialData: [],
-                builder: (_, snapshot) {
-                  if(snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: Text("Loading..."),
-                    );
-                  } else {
-                    return GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(context: context,
-                          builder: (context) {
-                            return Container(
-                              color: Color(0XFF737373),
-                              height: 500,
-                              child: Container(
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: StreamBuilder(
+                  //future: getNearby(),
+                  stream: DatabaseService(uid: user.userId).customerTickets,
+                  initialData: [],
+                  builder: (_, snapshot) {
+                    if(snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                        child: Text("Loading..."),
+                      );
+                    } else {
+                      return GestureDetector(
+                        onTap: () {
+                          showModalBottomSheet(context: context,
+                            builder: (context) {
+                              return Container(
+                                color: Color(0XFF737373),
+                                height: 500,
                                 child: Container(
-                                  child: QrImage(
-                                    data: user.userId,
-                                    size: 200,
-                                  ),
-                                ),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).canvasColor,
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: const Radius.circular(15),
-                                      topRight: const Radius.circular(15)
-                                  ),
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      child: ListView.builder(
-                          physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemCount: snapshot.data.length,
-                          itemBuilder: (_, index) {
-                            return Container(
-                              margin: EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(color: Colors.grey.withOpacity(0.8),
-                                      spreadRadius: 5.0,
-                                      blurRadius: 7.0,
-                                      offset: Offset(0, 3)),
-                                ],
-                                borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              height: 175.0,
-                              width: MediaQuery.of(context).size.width,
-                              child: Stack(
-                                children: <Widget>[
-                                  Container(
-                                    margin: EdgeInsets.all(10),
-                                    height: 200.0,
-//                                  width: MediaQuery.of(context).size.width * 0.3,
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.start,
-                                      crossAxisAlignment: CrossAxisAlignment.center,
-                                      children: <Widget>[
-                                        Column(
-                                          children: <Widget>[
-                                            QrImage(
-                                              data: user.userId,
-                                              size: 140,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
+                                  child: Container(
+                                    child: QrImage(
+                                      data: user.userId,
+                                      size: 150,
                                     ),
                                   ),
-                                  Positioned(
-                                    left: MediaQuery.of(context).size.width * 0.3 + 30,
-                                    top: 10,
-                                    child: Container(
-                                      child: Column(
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).canvasColor,
+                                    borderRadius: BorderRadius.only(
+                                        topLeft: const Radius.circular(15),
+                                        topRight: const Radius.circular(15)
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                        child: ListView.builder(
+                            physics: NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (_, index) {
+                              return Container(
+                                margin: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow: [
+                                    BoxShadow(color: Colors.grey.withOpacity(0.8),
+                                        spreadRadius: 5.0,
+                                        blurRadius: 7.0,
+                                        offset: Offset(0, 3)),
+                                  ],
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                height: 175.0,
+                                width: MediaQuery.of(context).size.width,
+                                child: Stack(
+                                  children: <Widget>[
+                                    Container(
+                                      margin: EdgeInsets.all(10),
+                                      height: 200.0,
+//                                  width: MediaQuery.of(context).size.width * 0.3,
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.start,
+                                        crossAxisAlignment: CrossAxisAlignment.center,
                                         children: <Widget>[
-                                          Text(
-                                            snapshot.data[index].businessName,
-                                            style: GoogleFonts.robotoSlab(
-                                              color: Colors.black,
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                          SizedBox(height: 20.0,),
-                                          Text(
-                                            "Your spot in q: ${snapshot.data[index].positionInLine}",
-                                            style: GoogleFonts.robotoSlab(
-                                              color: Colors.black,
-                                              fontSize: 25,
-                                              fontWeight: FontWeight.w500,
-                                            ),
+                                          Column(
+                                            children: <Widget>[
+                                              QrImage(
+                                                data: user.userId,
+                                                size: 140,
+                                              ),
+                                            ],
                                           ),
                                         ],
                                       ),
                                     ),
-                                  )
-                                ],
-                              ),
-                            );
-                          }
-                      ),
-                    );
-                  }
-                },
+                                    Positioned(
+                                      left: MediaQuery.of(context).size.width * 0.3 + 50,
+                                      top: 10,
+                                      child: Container(
+                                        child: Column(
+                                          children: <Widget>[
+                                            Text(
+                                              snapshot.data[index].businessName,
+                                              style: GoogleFonts.robotoSlab(
+                                                color: Colors.black,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                            SizedBox(height: 20.0,),
+                                            Text(
+                                              "Your spot in q: ${snapshot.data[index].positionInLine}",
+                                              style: GoogleFonts.robotoSlab(
+                                                color: Colors.black,
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              );
+                            }
+                        ),
+                      );
+                    }
+                  },
+                ),
               ),
-            ),
-          ],
-        ),
-      )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
